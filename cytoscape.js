89d9c10de5213b9909a23e3b669ba87992f66046
Roberto <robertomariani258@gmail.com>
2023-03-25 12:46:17 -0400

    added alternative distance vector algorithm

--- cytoscape.js
+++ cytoscape.js
@@ -261,9 +261,162 @@ graph.addEdge("B", "E", 9);
 graph.addEdge("C", "D", 4);
 graph.addEdge("D", "E", 2);
 
-
+//console.log(graph)
 // Find shortest path between A and E
 const result = dvAlgo(graph, "C", "E");
 console.log(result)
-console.log(`Shortest path: ${result.path.join(" -> ")}`);
-console.log(`Total distance: ${result.distance}`);
+// console.log(`Shortest path: ${result.path.join(" -> ")}`);
+// console.log(`Total distance: ${result.distance}`);
+
+/*
+-----------------------------------------------------------------------------------------------------------------
+*/
+/*Alternative distance vector algo implementation*/
+function initializeCostMatrix(adjacencyList){
+    let costMatrix = new Map()
+
+    for(let [vertex, neighbors] of adjacencyList.nodes) {
+        costMatrix.set(vertex, {})
+        costMatrix.get(vertex)['changed'] = true
+        costMatrix.get(vertex)['distanceVector'] = {}
+        costMatrix.get(vertex)['distanceVector'][vertex] = {'distance': 0, 'hop': vertex}
+        neighbors.forEach(neighbor => {
+            costMatrix.get(vertex)['distanceVector'][neighbor['node']] = {'distance': neighbor['weight'], 'hop': neighbor['node']}
+        })
+        
+    }
+
+    
+    //add non-neighbor vertices to the distance vector of each vertex
+    for(let key of adjacencyList.nodes.keys()) {
+        let distanceVector = costMatrix.get(key)['distanceVector']
+        for(let node of adjacencyList.nodes.keys()) {
+                if(distanceVector[node] === undefined) {
+                    distanceVector[node] = {'distance': Infinity, 'hop': null}
+                }
+        }
+
+    }
+
+
+    return costMatrix
+}
+
+function alternativeDVAlgo(adjacencyList) {
+    let costMatrix = initializeCostMatrix(adjacencyList)
+    console.log("Initially the distance vector of each vertex is the distance to each of its neighbours, and all non-neighbor vertices have a distance of infinity")
+    //while algorithm has not converged
+    while(!checkForConvergence(costMatrix)) {
+        let updatedCostMatrix = new Map();
+        console.log("Each node with an updated distance vector will share its distance vector with each of its neighbors")
+        for(let key of costMatrix.keys()){
+            let neighbors = adjacencyList.nodes.get(key)
+            let changedNeighbors = []
+
+            //iterate through neighbors and check if their cost matrix has changed
+            for(let neighbor of neighbors) {
+                if(costMatrix.get(neighbor['node'])['changed'] === true) {
+                  //if neighbors distance vector has changed add neighbor to list of neighbors whose distance vector has changed
+                  changedNeighbors.push(neighbor['node'])
+                }
+            }
+
+            let updatedDV = updateDistanceVector(key, changedNeighbors, costMatrix)
+            let currentDV = costMatrix.get(key)['distanceVector']
+            let annotation = "After "
+
+            if(neighbors.length > 0) {
+              for(let element of neighbors) {
+                //console.log(element['node'])
+                annotation += element['node'] + ", "
+              }
+              annotation += "share their distance vectors with " + key + ", " + key + " updates its distance vector";
+              console.log(annotation)
+            }
+            
+            if(checkIfDistanceVectorUpdated(currentDV, updatedDV)) {
+              updatedCostMatrix.set(key, {'changed':true, 'distanceVector': updatedDV})
+            } else {
+              updatedCostMatrix.set(key, {'changed':false, 'distanceVector': currentDV})
+            }
+        }
+        costMatrix = updatedCostMatrix
+    }
+    console.log("No updates from neighbor nodes have been sent, so now each node has the shortest distance to all other nodes in the network")
+    return costMatrix
+}
+
+function updateDistanceVector(currentNode, changedNeighbors, costMatrix){
+    let newDV = {}
+    for(let vertex of costMatrix.keys()) {
+      let minDis = Infinity
+      let hop = null
+      for(let neighbor of changedNeighbors) {
+
+        let distanceFromCurrentNodeToVertex = costMatrix.get(currentNode)['distanceVector'][vertex]['distance']
+        let distanceToNeighbor = costMatrix.get(currentNode)['distanceVector'][neighbor]['distance']
+        let distanceFromNeighborToVertex = costMatrix.get(neighbor)['distanceVector'][vertex]['distance']
+
+
+        if(minDis !== Infinity && distanceToNeighbor + distanceFromNeighborToVertex < minDis) {
+          minDis = distanceToNeighbor + distanceFromNeighborToVertex
+          hop = neighbor
+        }
+        else if(minDis === Infinity && distanceToNeighbor + distanceFromNeighborToVertex < distanceFromCurrentNodeToVertex) {
+          minDis = distanceToNeighbor + distanceFromNeighborToVertex
+          hop = neighbor
+        } 
+        
+      }
+      
+      //if minDis has not changed then distance from currentNode to vertex is already shortest so don't update distance
+      if(minDis === Infinity) {
+        newDV[vertex] = costMatrix.get(currentNode)['distanceVector'][vertex]
+      } else { //otherwise update distance to minimum distance
+        newDV[vertex] = {'distance': minDis, 'hop': hop}
+      }
+    }
+
+    return newDV;
+
+}
+
+function checkIfDistanceVectorUpdated(currentDV, newDV) {
+  for(let vertex of Object.keys(currentDV)) {
+    if(currentDV[vertex]['distance'] !== newDV[vertex]['distance']) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function checkForConvergence(costMatrix) {
+    for(let node of costMatrix.keys()) {
+        if(costMatrix.get(node)['changed'] === true) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function getPath(start, destination, minCostMatrix) {
+    let path = []
+    path.push(start)
+    let currentNode = start
+
+    while(currentNode != destination){
+      let nextNode = minCostMatrix.get(currentNode)['distanceVector'][destination]['hop']
+      path.push(nextNode)
+      currentNode = nextNode
+    }
+
+    return path;
+}
+
+
+let minCostMatrix = alternativeDVAlgo(graph)
+console.log(getPath('A', 'D', minCostMatrix))
+console.log(getPath('B', 'D', minCostMatrix))
+console.log(getPath('E', 'C', minCostMatrix))
+console.log('Shortest distance from E to C: ' + minCostMatrix.get('E')['distanceVector']['C']['distance'])
